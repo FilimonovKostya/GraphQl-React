b65804480ef2deb30267cc7ff1d0e3da41578f06
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-03-12 12:02:25 +0400

    link apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-   <div>
-     hidaddkd
-   </div>
-  );
+    return (
+        <div>
+            hidaddkd
+        </div>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3001/graphql',
+    cache: new InMemoryCache()
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
